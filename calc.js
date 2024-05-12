bdcefc5518ea2a2284706df2011582ea3e7f0c89
JosephEmeka <josephemekaonah@gmail.com>
2024-05-12 07:39:21 +0100

    Initial commit

---
+++ calc.js
@@ -0,0 +1,54 @@
+document.getElementById().onclick = function add() {
+    
+    
+}
+document.getElementById().onclick = function subtract() {
+    
+}
+document.getElementById().onclick = function multiply() {
+    
+}
+document.getElementById().onclick = function divide() {
+    
+}
+document.getElementById().onclick = function left_bracket() {
+    
+}
+document.getElementById().onclick = function right_bracket() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+document.getElementById().onclick = function add() {
+    
+}
+
+
