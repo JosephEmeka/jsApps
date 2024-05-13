73ee4acb55807f61b4e53dbce8c449992804296b
JosephEmeka <josephemekaonah@gmail.com>
2024-05-13 03:05:29 +0100

    edits

--- calc.js
+++ calc.js
@@ -1,54 +1,28 @@
-document.getElementById(console).onclick = function add() {
-    
-    
-}
-document.getElementById().onclick = function subtract() {
-    
-}
-document.getElementById().onclick = function multiply() {
-    
-}
-document.getElementById().onclick = function divide() {
-    
-}
-document.getElementById().onclick = function left_bracket() {
-    
-}
-document.getElementById().onclick = function right_bracket() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
-}
-document.getElementById().onclick = function add() {
-    
+
+const result = document.querySelector('display');
+const digits = document.querySelectorAll('[data-digit]');
+const operations = document.querySelectorAll('[data-operation]');
+const equalsBtn = document.querySelector('[data-equals]');
+const clearBtn = document.querySelector('[data-clear]');
+
+digits.forEach(digit => digit.addEventListener('click', function() { appendToScreen(digit.textContent); }));
+operations.forEach(operation => operation.addEventListener('click', function() { appendToScreen(operation.textContent); }));
+equalsBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', clearScreen);
+dotBtn.addEventListener('click', function() { appendToScreen('.'); });
+
+function appendToScreen(value) {
+    document.getElementById('display').value += value;
 }
 
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
 
+function clearScreen() {
+    document.getElementById('display').value = '';
+}
